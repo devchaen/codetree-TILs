513dd32bb10f1455547f616c5647c58e604bc3ee
Chaehyeon Kim <101796518+devchaen@users.noreply.github.com>
2023-12-21 02:01:19 +0900

    Title: 좌우로 반복하며 출력, Time: 226ms, Memory: 45MB, Status: Wrong Answer - CodeTree

---
+++ 231221/좌우로 반복하며 출력/print-in-left-right-reverse.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let ans = [];
+
+for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) {
+        let str = '';
+        for (let j = 0; j < n; j++) {
+            str += `${j + 1}`
+        }
+        ans.push(str);
+    } else {
+        let str = '';
+        for (let k = 0; k < n; k++) {
+            str += `${4 - k}`
+        }
+        ans.push(str);
+    }
+}
+
+console.log(ans.join('\n'));
